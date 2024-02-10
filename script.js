ece8d551626e023068483c04549d6776b9674b5a
Ivan Nomokonov <inomokonov@it-one.ru>
2024-02-11 00:02:38 +0300

    адаптировал под мобильные устройства

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const header = document.querySelector("header");
+const containerFluid = document.querySelector(
+  "header .navbar.navbar-expand-lg"
+);
+let menuShow = false;
 
 document.addEventListener("scroll", (e) => {
   if (document.scrollingElement.scrollTop < 200) {
@@ -6,4 +10,40 @@ document.addEventListener("scroll", (e) => {
     return;
   }
   header.classList.add("dark");
+});
+
+const menuBtn = document.querySelector(".navbar-toggler");
+
+menuBtn.addEventListener("click", () => {
+  document.querySelector(".navbar-collapse.collapse").classList.toggle("show");
+  menuBtn.classList.toggle("collapsed");
+  menuBtn.setAttribute(
+    "aria-expanded",
+    menuBtn.classList.contains("collapsed") ? false : true
+  );
+
+  containerFluid.classList.toggle("show");
+
+  menuShow = !menuShow;
+});
+
+document.addEventListener("click", (e) => {
+  if (!menuShow) {
+    return;
+  }
+
+  if (e.target.closest(".navbar")) {
+    return;
+  }
+
+  document.querySelector(".navbar-collapse.collapse").classList.toggle("show");
+  menuBtn.classList.toggle("collapsed");
+  menuBtn.setAttribute(
+    "aria-expanded",
+    menuBtn.classList.contains("collapsed") ? false : true
+  );
+
+  containerFluid.classList.toggle("show");
+
+  menuShow = !menuShow;
 });
